61c58fa556e87f2f01b93fc167023d836fcec04b
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-10 00:16:58 +0300

    Create assets app urls.py

---
+++ backend/apps/assets/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AssetCategoryViewSet, AssetViewSet
+
+router = DefaultRouter()
+router.register(r'asset-categories', AssetCategoryViewSet)
+router.register(r'assets', AssetViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
