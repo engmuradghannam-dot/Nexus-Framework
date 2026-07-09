2d11d66eb3cb225263fbde9166b75579c14c33c2
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-09 23:45:01 +0300

    Fix regulatory/signals.py: ComplianceRule/AuditLog -> ComplianceCheck (match models.py)

--- backend/apps/regulatory/signals.py
+++ backend/apps/regulatory/signals.py
@@ -1,13 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import ComplianceRule, AuditLog
+from .models import ComplianceCheck
 
 
-@receiver(post_save, sender=ComplianceRule)
-def handle_rule_change(sender, instance, created, **kwargs):
-    if not created:
-        AuditLog.objects.create(
-            rule=instance,
-            action='updated',
-            details=f'Rule {instance.name} was modified'
-        )
+@receiver(post_save, sender=ComplianceCheck)
+def handle_compliance_check(sender, instance, created, **kwargs):
+    pass
